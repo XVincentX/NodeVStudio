f59eea547c78d788b710bc45a41eb50e19a60774
Vincenzo Chianese <v.chianese@nettunosolutions.com>
2014-07-13 11:44:25 +0200

    Demo commit

---
+++ NodeVStudio/NodeVStudio/gulpfile.js
@@ -0,0 +1,34 @@
+﻿var gulp = require('gulp');
+var angularHtmlify = require('gulp-angular-htmlify');
+var angularFilesort = require('gulp-angular-filesort');
+var inject = require('gulp-inject');
+var angularTemplates = require('gulp-angular-templatecache');
+var es = require('event-stream');
+var minifyHTML = require('gulp-minify-html');
+
+gulp.task('htmlViews', function () {
+    return gulp.src('app/**/*.html')
+         .pipe(minifyHTML({ quotes: true }))
+         .pipe(angularHtmlify())
+         .pipe(angularTemplates('Views.js', { module: 'mantis', standalone: false, root: 'app' }))
+         .pipe(gulp.dest('./build'));
+});
+
+gulp.task('scripts', ['htmlViews'], function () {
+    return gulp.src('./index.html')
+      .pipe(inject(es.merge(gulp.src(['./bower_components/**/*.js', '!./bower_components/**/*.min.js', './app/app.js', './build/Views.js'], { read: false }), gulp.src(['./app/**/*.js', '!./app/**/*.no.js', '!./app/app.js']).pipe(angularFilesort()))))
+      .pipe(minifyHTML({ quotes: true }))
+      .pipe(gulp.dest('./build'));
+});
+
+gulp.task('shared', function () {
+    // place code for your default task here
+});
+
+gulp.task('Debug', ['shared'], function () {
+    // place code for your default task here
+});
+
+gulp.task('Release', ['shared', 'htmlViews', 'scripts'], function () {
+    // place code for your default task here
+});
