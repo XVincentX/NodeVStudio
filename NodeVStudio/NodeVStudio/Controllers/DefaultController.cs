0dae3da02a0ce4a13d7222b5338e837a24626b11
Vincenzo Chianese <v.chianese@nettunosolutions.com>
2014-07-14 10:50:16 +0200

    Added sample controller

---
+++ NodeVStudio/NodeVStudio/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NodeVStudio.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new { ID = 0, Text = "Hello" });
+        }
+    }
+}
---
+++ NodeVStudio/NodeVStudio/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin;
+using NodeVStudio;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Startup), "Start")]
+
+namespace NodeVStudio
+{
+
+    public class Startup
+    {
+        public void Start(IAppBuilder builder)
+        {
+            var configuration = new HttpConfiguration();
+
+            configuration.Formatters.Clear();
+            configuration.Formatters.Add(new JsonMediaTypeFormatter());
+            configuration.Formatters.Add(new BsonMediaTypeFormatter());
+
+            configuration.Routes.IgnoreRoute("axdIgnore", "{resource}.axd/{*pathInfo}");
+
+            configuration.Routes.MapHttpRoute(
+                     name: "API Default",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+
+            );
+
+            builder.UseWebApi(configuration);
+
+        }
+    }
+}
